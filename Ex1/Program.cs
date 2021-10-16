0eb4d8400baa38c051ad4516bdfb3692df8b4853
Sergei Kanter <sdkanter@outlook.com>
2021-10-16 17:19:21 +0200

    Добавьте файлы проекта.

---
+++ Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uravnenie;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Uravn uravnenie = new Uravn(k, b);
+            uravnenie.Root();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Uravnenie/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uravnenie
+{
+    public struct Uravn
+    {
+        public Uravn(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public double k;
+        public double b;
+
+        public void Root()
+        {
+            Console.WriteLine("Peremennaja X = {0}", -b / k);
+        }
+
+    }
+}
